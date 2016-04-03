a4ff9d7056cc0ee8576c6d10cbc0c62f0fe76a40
n15001 <n15001@std.it-college.ac.jp>
2016-04-04 01:35:45 +0900

    タイプミス修正

--- bounsegame.py
+++ bounsegame.py
@@ -7,7 +7,7 @@ import time
 tk = Tk()
 tk.title("Game")
 tk.resizable(0,0)
-tk.wm_attribustes("-topmost", 1)
+tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
