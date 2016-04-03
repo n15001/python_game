790643ae0f879f61d80092759f3ba4842e635259
n15001 <n15001@std.it-college.ac.jp>
2016-04-04 08:01:07 +0900

    教科書16.3

--- stickmangame.py
+++ stickmangame.py
@@ -11,6 +11,57 @@ class Coords:
         self.x2 = x2
         self.y2 = y2
 
+def within_x(co1, co2):
+    if co1.x1 > co2.x1 and co1.x1 < co2.x2:
+        return True
+    elif col.x2 > co2.x1 and co1.x2 < co2.x2:
+        return True
+    elif co2.x1 > co1.x1 and co2.x1 < co1.x2:
+        return True
+    elif co2.x2 > co1.x1 and co2.x2 < co1.x2:
+        return True
+    else:
+        return False
+
+def within_y(co1, co2):
+    if co1.y1 > co2.y1 and co1.y1 < co2.y2:
+        return True
+    elif col.y2 > co2.y1 and co1.y2 < co2.y2:
+        return True
+    elif co2.y1 > co1.y1 and co2.y1 < co1.y2:
+        return True
+    elif co2.y2 > co1.y1 and co2.y2 < co1.y2:
+        return True
+    else:
+        return False
+
+def collided_left(co1, co2):
+    if within_y(co1, co2):
+        if co1.x1 <= co2.x2 and co1.x1 >= co2.x1:
+            return True
+        return False
+
+def collided_right(co1, co2):
+    if within_y(co1, co2):
+        if co1.x2 <= co2.x1 and co1.x2 >= co2.x2:
+            return True
+        return False
+
+def collided_top(co1, co2):
+    if within_x(co1, co2):
+        if co1.y1 <= co2.y2 and co1.y1 >= co2.y1:
+            return True
+        return False
+
+def collided_bottom(y, co1, co2):
+    if within_x(co1, co2):
+        y_calc = co1.y2 + y
+        if y_calc >= co2.y1 and y_calc <= co2.y2:
+            return True
+        return False
+
+
+
 class Game:
     def __init__(self):
         self.tk = Tk()
