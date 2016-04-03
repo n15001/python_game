6e9a7ed091dde3436f3cc50626e042a392a48856
n15001 <n15001@std.it-college.ac.jp>
2016-04-04 07:45:44 +0900

    教科書16.2

--- stickmangame.py
+++ stickmangame.py
@@ -4,6 +4,13 @@ from tkinter import *
 import random
 import time
 
+class Coords:
+    def __init__(self, x1=0, y1=0, x2=0, y2=0):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
 class Game:
     def __init__(self):
         self.tk = Tk()
