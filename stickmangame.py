3c82e226e8b02aca5880d9c7188a5ae0e298d4f4
n15001 <n15001@std.it-college.ac.jp>
2016-04-04 09:48:36 +0900

    教科書17.1

--- stickmangame.py
+++ stickmangame.py
@@ -77,6 +77,24 @@ class PlatformSprite(Sprite):
         self.image = game.canvas.create_image(x, y, image=self.photo_image, anchor='nw')
         self.coordinates = Coords(x, y, x + width, y + height)
 
+class StickFigureSprite(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self, game)
+        self.images_left = [ PhotoImage(file="1.gif"), PhotoImage(file="2.gif"), PhotoImage(file="3.gif") ]
+        self.images_right = [ PhotoImage(file="1r.gif"), PhotoImage(file="2r.gif"), PhotoImage(file="3r.gif") ]
+        self.image = game.canvas.create_image(200, 470, image=self.images_left[0], anchor='nw')
+        self.x = -2
+        self.y = 0
+        self.current_image = 0
+        self.current_image_add = 1
+        self.jump_count = 0
+        self.last_time = time.time()
+        self.coordinates = Coords()
+        game.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        game.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        game.canvas.bind_all('<space>', self.jump)
+
+
 
 class Game:
     def __init__(self):
